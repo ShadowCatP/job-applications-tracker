b54420f1cd00820b581c172591fe773d5ab6a988
ShadowCatP <282276@student.pwr.edu.pl>
2025-04-13 00:19:14 +0200

    added user auth logic

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const cors = require("cors");
+const authRoutes = require("./routes/authRoutes");
 
 dotenv.config();
 const app = express();
@@ -9,8 +10,15 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use("/api/auth", authRoutes);
+
 app.get("/echo", (req, res) => res.send("Api is running..."));
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`));
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`));
+  })
+  .catch((err) => console.error(err));
