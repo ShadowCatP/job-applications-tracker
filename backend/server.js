e0b7a409fdf60d97f774b39295cf5b7e1e9d5e5e
ShadowCatP <282276@student.pwr.edu.pl>
2025-04-12 09:20:47 +0200

    added cors and echo route

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,16 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const cors = require("cors");
 
+dotenv.config();
 const app = express();
 
-app.get("/", (req, res) => res.send("Api is running..."));
+app.use(cors());
+app.use(express.json());
+
+app.get("/echo", (req, res) => res.send("Api is running..."));
 
 const PORT = process.env.PORT || 5000;
-try {
-  app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`));
-} catch (err) {
-  console.error(err);
-}
+
+app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`));
