9515a5dd901e9998e79235902f0cbab9dd8e9ab5
ShadowCatP <282276@student.pwr.edu.pl>
2025-04-13 13:43:23 +0200

    added user already exists error

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -4,10 +4,22 @@ const jwt = require("jsonwebtoken");
 
 exports.register = async (req, res) => {
   const { name, email, password } = req.body;
-  const hashed = await bcrypt.hash(password, 10);
-  const user = await User.create({ name, email, password: hashed });
-  const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
-  res.json({ user, token });
+
+  try {
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ msg: "User already exists" });
+    }
+
+    const hashed = await bcrypt.hash(password, 10);
+    const user = await User.create({ name, email, password: hashed });
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+
+    res.json({ user, token });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ msg: "Server error" });
+  }
 };
 
 exports.login = async (req, res) => {
