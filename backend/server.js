3d64724bf011166194b05cdb5705516c2334801b
mikalay <kolyatepl@gmail.com>
2025-04-19 22:36:35 +0200

    Fix docker-compose and token issues, needs further polishing

--- backend/server.js
+++ backend/server.js
@@ -19,8 +19,9 @@ app.get("/echo", (req, res) => res.send("Api is running..."));
 
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`));
+  })
+  .catch((err) => console.error(err));
