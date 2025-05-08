65617f5a558c8f633a191f34fcc6f506d24b99b1
ShadowCatP <282276@student.pwr.edu.pl>
2025-05-08 13:54:50 +0200

    added link to main page to the header

--- frontend/components/Header/Header.tsx
+++ frontend/components/Header/Header.tsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 import { Logout } from "../ui/logout";
 import { usePathname } from "next/navigation";
 import Logo from "./logo.png";
+import Link from "next/link";
 
 export const Header = () => {
   const pathname = usePathname();
@@ -14,13 +15,15 @@ export const Header = () => {
     <>
       {!hideHeader && (
         <header className="flex justify-between bg-white px-4 py-4 shadow">
-          <Image
-            src={Logo}
-            width={1024}
-            height={1024}
-            alt="Logo"
-            className="h-8 w-8"
-          />
+          <Link href={"/"}>
+            <Image
+              src={Logo}
+              width={1024}
+              height={1024}
+              alt="Logo"
+              className="h-8 w-8"
+            />
+          </Link>
           <Logout />
         </header>
       )}
