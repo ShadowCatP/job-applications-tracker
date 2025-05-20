05803821aef230c3ff1069bbad0761baf35ec327
ShadowCatP <282276@student.pwr.edu.pl>
2025-05-20 09:19:46 +0200

    changed theme handling to next-themes

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -25,9 +25,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased transition-colors dark:bg-neutral-900 dark:text-neutral-100`}
       >
         <Providers>
           <Header />
--- frontend/app/providers.tsx
+++ frontend/app/providers.tsx
@@ -1,8 +1,15 @@
 "use client";
 
 import { AuthProvider } from "@/context/AuthContext";
+import { ThemeProvider } from "next-themes";
 import { ReactNode } from "react";
 
 export default function Providers({ children }: { children: ReactNode }) {
-  return <AuthProvider>{children}</AuthProvider>;
+  return (
+    <AuthProvider>
+      <ThemeProvider attribute={"class"} defaultTheme={"system"} enableSystem>
+        {children}
+      </ThemeProvider>
+    </AuthProvider>
+  );
 }
